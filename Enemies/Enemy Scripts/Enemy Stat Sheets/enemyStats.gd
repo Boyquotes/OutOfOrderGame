efaad06ce78da3bf5e36267190a3ec196948e57b
jacobartis <ZJAC219@live.rhul.ac.uk>
2022-07-27 15:03:38 +0100

    Added damage and atack delay stats to the stat sheets instead of enemies using the gun stats, this it to allow for better control over game balance.

--- Enemies/Enemy Scripts/Enemy Stat Sheets/enemyStats.gd
+++ Enemies/Enemy Scripts/Enemy Stat Sheets/enemyStats.gd
@@ -6,3 +6,5 @@ export var weapon: PackedScene
 export var turn_speed: float = 4.0
 export var idle_turn_delay: float = 3.0
 export var move_speed: float = 2.0
+export var damage: float = 10.0
+export var attack_delay: float = 0.5
