927308550e950525f4bd86013d018b2be71bd158
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-05-19 14:20:54 +0100

    working on magnitism and weapon rework.

--- Scripts/Player/player_controller.gd
+++ Scripts/Player/player_controller.gd
@@ -14,7 +14,7 @@ const FRICTION = 8
 @onready var aim_ray = $Camera3D/AimRay
 @onready var hand = $Camera3D/SubViewportContainer/SubViewport/WeaponCam/Hand
 @onready var weapon_cam = $Camera3D/SubViewportContainer/SubViewport/WeaponCam
-@onready var magnet_area = $Camera3D/MagnetArea
+@onready var weapon_magnet_system = $Camera3D/WeaponMagnetSystem
 @onready var animation_player = $AnimationPlayer
 @onready var debug_label = $CanvasLayer/Label
 
@@ -25,7 +25,6 @@ var max_ground_speed: float = 8
 var max_air_speed: float = 4
 var acceleration: float = 3
 var jump_strength: float = 5
-var dash_speed: float = 30
 
 #Health Variables
 var max_health: float = 100
@@ -34,32 +33,23 @@ var max_health: float = 100
 #Energy Variables
 var max_energy: float = 5
 @onready var energy: float = get_max_energy()
-var dash_cost: float = 1
-var pull_cost: float = .5
+var magnet_cost: float = 2
 
 #Magnetic Variables
 var magnetic_bodies: Array = []
-var pull_strength: float = 15
+var magnet_strength: float = 10
+var dash_cost: float = 2.5
 
 #Getters
 
 func get_state_controller() -> Node:
 	return state_controller
 
-func get_camera() -> Camera3D:
-	return camera
-
-func get_shader() -> ShaderMaterial:
-	return shader
-
-func get_shader_pix_size() -> float:
-	return shader.get_shader_parameter("pixelSize")
-
-func get_interact_raycast() -> RayCast3D:
-	return interact_ray
-
 func get_animation_player() -> AnimationPlayer:
 	return animation_player
+
+func get_camera() -> Camera3D:
+	return camera
 
 func get_weapon() -> Node3D:
 	return weapon
@@ -79,9 +69,6 @@ func get_jump_strength() -> float:
 func get_friction() -> float:
 	return FRICTION
 
-func get_dash_speed() -> float:
-	return dash_speed
-
 func get_max_health() -> float:
 	return max_health
 
@@ -94,13 +81,12 @@ func get_max_energy() -> float:
 func get_energy() -> float:
 	return energy
 
+func get_magnet_cost() -> float:
+	return magnet_cost
+
 func get_dash_cost() -> float:
 	return dash_cost
-
 #Setters
-
-func set_shader_pix_size(value:float) -> void:
-	shader.set_shader_parameter("pixelSize", value)
 
 func set_weapon(value:Node3D) -> void:
 	weapon = value
@@ -117,9 +103,6 @@ func set_acceleration(value:float) -> void:
 func set_jump_strength(value:float) -> void:
 	jump_strength = value
 
-func set_dash_speed(value:float) -> void:
-	dash_speed = value
-
 func set_max_health(value:float) -> void:
 	max_health = value
 
@@ -132,14 +115,10 @@ func set_max_energy(value:float) -> void:
 func set_energy(value:float) -> void:
 	energy = value
 
-func set_dash_cost(value:float) -> void:
-	dash_cost = value
-
 #Functions
 
 func _ready() -> void:
 	state_controller.init(self)
-	
 	#Debug stuff
 	for child in hand.get_children():
 		if !child is Weapon:
@@ -150,6 +129,7 @@ func _ready() -> void:
 func _process(delta) -> void:
 	state_controller.process(delta)
 	weapon_cam.set_transform(camera.get_global_transform())
+	debug_label.set_text(str(get_energy()))
 	set_weapon_aim()
 
 func set_weapon_aim() -> void:
@@ -185,15 +165,7 @@ func pickup_weapon(new_weapon:RigidBody3D):
 		return
 	
 	#Removes previous weapon
-	if get_weapon():
-		var old_weapon = get_weapon()
-		hand.remove_child(old_weapon)
-		find_parent("QodotMap").call_deferred("add_child",old_weapon)
-		old_weapon.set_collectable(true)
-		old_weapon.set_position(get_position())
-		var cam_rot = camera.get_global_rotation().normalized()
-		old_weapon.call_deferred("throw",Vector3(0,0,-5).rotated(Vector3(1,0,0),cam_rot.x).rotated(Vector3(0,1,0),get_rotation().y).rotated(Vector3(0,0,1),cam_rot.z))
-	
+	drop_weapon()
 	#Equips new weapon
 	if !new_weapon.get_parent():
 		hand.add_child(new_weapon)
@@ -201,9 +173,21 @@ func pickup_weapon(new_weapon:RigidBody3D):
 		new_weapon.get_parent().remove_child(new_weapon)
 		hand.add_child(new_weapon)
 	new_weapon.set_global_transform(hand.get_global_transform())
-	new_weapon.set_held(true)
+#	new_weapon.set_held(true)
 	new_weapon.set_model_layer(2)
 	set_weapon(new_weapon)
+
+func drop_weapon() -> void:
+	weapon = get_weapon()
+	
+	if !weapon:
+		return
+	
+	hand.remove_child(weapon)
+	find_parent("QodotMap").call_deferred("add_child",weapon)
+	weapon.set_global_transform(camera.get_global_transform())
+	weapon.set_equiped(false)
+	weapon.push(get_position(),1)
 
 #Handles calling attack on held weapon
 func attack() -> void:
@@ -233,18 +217,24 @@ func throw() -> void:
 	get_weapon().throw(Vector3(0,0,-35).rotated(Vector3(1,0,0),cam_rot.x).rotated(Vector3(0,1,0),get_rotation().y).rotated(Vector3(0,0,1),cam_rot.z))
 	set_weapon(null)
 
-func pull_weapons() -> void:
-	set_energy(get_energy()-pull_cost)
-	for body in magnetic_bodies:
-		if body is RigidBody3D:
-			var time = .5
-			var pos = camera.get_global_position()+Vector3(0,1,0)+get_velocity()*time
-			body.apply_central_impulse(body.get_position().direction_to(pos).normalized()*pull_strength)
-	
+#func pull_magnet(delta) -> void:
+#	set_energy(get_energy()-get_magnet_cost()*delta)
+#	for body in magnetic_bodies:
+#		body.magnetic_pull(get_global_position(),magnet_strength)
+
+func pull_magnet(delta) -> void:
+	weapon_magnet_system.pull()
 
 #Handles taking damage
 func take_damage(damage) -> void:
 	set_health(clamp(get_health()-damage,0,INF))
+
+#Handles healing returns false if the player cant be healed
+func heal(heal_quant) -> bool:
+	if get_health()==get_max_health():
+		return false
+	set_health(clamp(get_health()+heal_quant,0,get_max_health()))
+	return true
 
 #Signal Functions
 
@@ -259,3 +249,7 @@ func _on_magnet_area_body_exited(body):
 func _on_auto_pickup_area_entered(area):
 	if area.is_in_group("Weapon") and !get_weapon():
 		pickup_weapon(area.get_parent())
+
+func _on_auto_pickup_body_entered(body):
+	if body.is_in_group("Collectable"):
+		body.collect(self)
